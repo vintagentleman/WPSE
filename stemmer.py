f27f8d0436e560897d12e9618d848110a1903a0c
Konstantin Sipunin <79818514079@yandex.ru>
2017-09-06 14:09:38 +0300

    Add functions which process tokens rather than whole strings, expand unit testing, major fixes.

--- stemmer.py
+++ stemmer.py
@@ -2,7 +2,7 @@ from tokeniser import tokenise
 
 
 infl_glob = {
-    # Окончания второго склонения
+    # Окончания второго склонения (стандартные)
     'а', 'и', 'е', 'у', 'ой', 'ою', 'ам', 'ами', 'ах',
 }
 
@@ -20,15 +20,17 @@ def stemmer(s):
         if token.kind == 'ALPHABETIC':
             t = token.string
 
-            # Выделяем множество его конечных подстрок длины, не превосходящей max_len
-            infl_opts = set(t[-i:] for i in range(max_len + 1))
-
-            # Пересекаем его со словарём флексий и сортируем по возрастанию длины
-            infl = sorted(infl_glob & infl_opts, key=lambda x: len(x))
+            # Выделяем множество его конечных подстрок длины, не превосходящей max_len;
+            # затем пересекаем его со словарём флексий и сортируем по возрастанию длины
+            infl = sorted(set(t[-i:] for i in range(max_len + 1) if t[-i:] in infl_glob), key=lambda x: len(x))
 
             if infl:
-                for fl in infl:
-                    d.setdefault(t, []).append((t[:-len(fl)], fl))
+                # Особо учитываем случай, когда основа совпала с флексией
+                if len(infl) == 1 and infl[0] == t:
+                    d.setdefault(t, []).append((t, ''))
+                else:
+                    for fl in infl:
+                        d.setdefault(t, []).append((t[:-len(fl)], fl))
             else:
                 d.setdefault(t, []).append((t, ''))
 
@@ -43,13 +45,55 @@ def greedy_stemmer(s):
     for token in tokenise(s):
         if token.kind == 'ALPHABETIC':
             t = token.string
+            infl = ''
 
-            infl_opts = set(t[-i:] for i in range(max_len + 1))
-            infl = sorted(infl_glob & infl_opts, key=lambda x: len(x))
+            # Здесь перебирать всё не нужно, берём только флексию наибольшей длины
+            for i in range(max_len, 0, -1):
+                if t[-i:] in infl_glob:
+                    infl = t[-i:]
+                    break
 
             if infl:
-                d[t] = t[:-len(infl[-1])]
+                if infl == t:
+                    d[t] = t
+                else:
+                    d[t] = t[:-len(infl)]
             else:
                 d[t] = t
 
     return d
+
+
+def stemmer_by_token(t):
+
+    max_len = max(len(fl) for fl in infl_glob)
+    s = t.string
+    infl = sorted(set(s[-i:] for i in range(max_len + 1) if s[-i:] in infl_glob), key=lambda x: len(x))
+
+    if infl:
+        if len(infl) == 1 and infl[0] == s:
+            return s,
+        else:
+            return tuple(s[:-len(fl)] for fl in infl)
+    else:
+        return s,
+
+
+def greedy_stemmer_by_token(t):
+
+    max_len = max(len(fl) for fl in infl_glob)
+    s = t.string
+    infl = ''
+
+    for i in range(max_len, 0, -1):
+        if s[-i:] in infl_glob:
+            infl = s[-i:]
+            break
+
+    if infl:
+        if infl == s:
+            return s
+        else:
+            return s[:-len(infl)]
+    else:
+        return s
--- stemmer_test.py
+++ stemmer_test.py
@@ -1,5 +1,6 @@
 import unittest
 import stemmer
+from tokeniser import Token
 
 
 class TestStemmer(unittest.TestCase):
@@ -11,8 +12,20 @@ class TestStemmer(unittest.TestCase):
             'мама': [('мам', 'а')],
             'мамами': [('мамам', 'и'), ('мам', 'ами')],
             'мамань': [('мамань', '')],
-            'а': [('', 'а')],
+            'а': [('а', '')],
         })
+
+        ana = stemmer.stemmer_by_token(Token('мама'))
+        self.assertEqual(ana, ('мам',))
+
+        ana = stemmer.stemmer_by_token(Token('мамами'))
+        self.assertEqual(ana, ('мамам', 'мам',))
+
+        ana = stemmer.stemmer_by_token(Token('мамань'))
+        self.assertEqual(ana, ('мамань',))
+
+        ana = stemmer.stemmer_by_token(Token('а'))
+        self.assertEqual(ana, ('а',))
 
     def test_greedy(self):
         ana = stemmer.greedy_stemmer('папа, папами, папань, и')
@@ -21,8 +34,28 @@ class TestStemmer(unittest.TestCase):
             'папа': 'пап',
             'папами': 'пап',
             'папань': 'папань',
-            'и': '',
+            'и': 'и',
         })
+
+        ana = stemmer.greedy_stemmer_by_token(Token('папа'))
+        self.assertEqual(ana, 'пап')
+
+        ana = stemmer.greedy_stemmer_by_token(Token('папами'))
+        self.assertEqual(ana, 'пап')
+
+        ana = stemmer.greedy_stemmer_by_token(Token('папань'))
+        self.assertEqual(ana, 'папань')
+
+        ana = stemmer.greedy_stemmer_by_token(Token('и'))
+        self.assertEqual(ana, 'и')
+
+    def test_empty(self):
+        self.assertEqual(stemmer.stemmer(''), {})
+        self.assertEqual(stemmer.greedy_stemmer(''), {})
+
+        # Таких токенов просто быть не может
+        # self.assertEqual(stemmer.stemmer_by_token(Token('')), ('',))
+        # self.assertEqual(stemmer.greedy_stemmer_by_token(Token('')), '')
 
 
 if __name__ == '__main__':
